75f40cb1af78aecfb4862a1135628b50421a41d1
jordaninthewind <jordan.kline@gmail.com>
2026-05-04 17:58:39 -0700

    revert rpi

--- app/sensors/motion.py
+++ app/sensors/motion.py
@@ -1,24 +1,21 @@
 from datetime import datetime, timezone
 from typing import Callable
-import RPi.GPIO as GPIO
+from gpiozero import MotionSensor as _PIR
 from .base import Sensor
 import config
 
 
 class MotionSensor(Sensor):
     def __init__(self, pin: int = config.MOTION_SENSOR_PIN):
-        self._pin = pin
+        self._sensor = _PIR(pin)
         self._last_triggered: str | None = None
         self._callbacks: list[Callable[[dict], None]] = []
-
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self._pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self._pin, GPIO.RISING, callback=self._on_motion, bouncetime=300)
+        self._sensor.when_motion = self._on_motion
 
     def on_trigger(self, callback: Callable[[dict], None]) -> None:
         self._callbacks.append(callback)
 
-    def _on_motion(self, _channel: int) -> None:
+    def _on_motion(self) -> None:
         self._last_triggered = datetime.now(timezone.utc).isoformat()
         data = self.read()
         for cb in self._callbacks:
@@ -26,10 +23,6 @@ class MotionSensor(Sensor):
 
     def read(self) -> dict:
         return {
-            "motion": bool(GPIO.input(self._pin)),
+            "motion": self._sensor.motion_detected,
             "last_triggered": self._last_triggered,
         }
-
-    def cleanup(self) -> None:
-        GPIO.remove_event_detect(self._pin)
-        GPIO.cleanup(self._pin)
