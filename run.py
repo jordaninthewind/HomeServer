6ac316a8ef331d6431921673e59ba3bd27b2847f
jordaninthewind <jordan.kline@gmail.com>
2026-05-05 20:41:47 -0700

    add nodemon support to environment

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app import create_app, socketio
 import config
+from werkzeug.debug import DebuggedApplication
 
-app = create_app()
+app = DebuggedApplication(create_app(), allow_unsafe_werkzeug=True)
 
 if __name__ == "__main__":
     socketio.run(app, host=config.HOST, port=config.PORT, debug=config.DEBUG)
